fff61cd20afcd858778936b4bc9b0121c3b371ed
Paul Bouloc <37270137+paul-bouloc@users.noreply.github.com>
2025-09-28 00:52:09 +0200

    fixed birthdate policy

--- apps/api/src/core/validation/policies/birthdate.policy.ts
+++ apps/api/src/core/validation/policies/birthdate.policy.ts
@@ -1,11 +1,14 @@
 import z from 'zod'
 
-export const BirthDatePolicy = z.string().refine(
-  (val) => {
-    const date = new Date(val)
-    return date < new Date() && date > new Date(new Date().setFullYear(new Date().getFullYear() - 100))
-  },
-  {
-    message: 'Birth date must be before today and less than 100 years ago',
-  },
-)
+export const BirthDatePolicy = z.iso
+  .date('Invalid birth date')
+  .refine(
+    (val) => {
+      const date = new Date(val)
+      return date < new Date() && date > new Date(new Date().setFullYear(new Date().getFullYear() - 100))
+    },
+    {
+      message: 'Birth date must be before today and less than 100 years ago',
+    },
+  )
+  .transform((val) => new Date(val))
