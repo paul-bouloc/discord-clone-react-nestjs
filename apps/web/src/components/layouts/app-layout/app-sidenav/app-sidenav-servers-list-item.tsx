c4c474a57bbb46881e21bd4e3184caea0602d776
Paul Bouloc <37270137+paul-bouloc@users.noreply.github.com>
2025-10-22 01:07:26 +0200

    fix siver list item tooltip

--- apps/web/src/components/layouts/app-layout/app-sidenav/app-sidenav-servers-list-item.tsx
+++ apps/web/src/components/layouts/app-layout/app-sidenav/app-sidenav-servers-list-item.tsx
@@ -19,47 +19,49 @@ export const AppSidenavServersListItem = ({
   tooltip,
 }: AppSidenavServersListItemProps) => {
   return (
-    <div className="group relative flex h-10 w-full cursor-pointer justify-center">
-      {/* ETA MARK */}
-      <div className="absolute top-0 left-0 w-1">
-        {/* Marker sélectionné - pleine hauteur */}
-        {isSelected && showSelectedMarker && (
-          <div className="bg-gray-230 flex h-10 items-center justify-center rounded-r-full"></div>
-        )}
-
-        {/* Marker hover - demi-hauteur (seulement si pas sélectionné) */}
-        {!isSelected && showSelectedMarker && (
-          <div className="bg-gray-230 mt-2.5 flex h-5 items-center justify-center rounded-r-full opacity-0 transition-opacity duration-200 group-hover:opacity-100"></div>
-        )}
-      </div>
-      {/* BOX */}
-
+    <>
       <Tooltip>
         <TooltipTrigger asChild>
-          <button
-            className={cn(
-              'flex h-10 w-10 shrink-0 cursor-pointer items-center justify-center overflow-hidden rounded-lg transition-all duration-200',
-              isSelected && 'bg-brand-500',
-              !isSelected && 'group-hover:bg-brand-500 bg-gray-700',
-            )}
-          >
-            {serverAvatarSrc ? (
-              <img src={serverAvatarSrc} alt="server avatar" className="h-full w-full object-cover object-center" />
-            ) : avatarFallback ? (
-              <p>{avatarFallback}</p>
-            ) : icon ? (
-              icon
-            ) : (
-              <img src="/src/assets/discord-mark-white.svg" alt="discord logo" className="w-[22.8px]" />
-            )}
+          <button className="group relative flex h-10 w-full cursor-pointer justify-center">
+            {/* ETA MARK */}
+            <div className="absolute top-0 left-0 w-1">
+              {/* Marker sélectionné - pleine hauteur */}
+              {isSelected && showSelectedMarker && (
+                <div className="bg-gray-230 flex h-10 items-center justify-center rounded-r-full"></div>
+              )}
+
+              {/* Marker hover - demi-hauteur (seulement si pas sélectionné) */}
+              {!isSelected && showSelectedMarker && (
+                <div className="bg-gray-230 mt-2.5 flex h-5 items-center justify-center rounded-r-full opacity-0 transition-opacity duration-200 group-hover:opacity-100"></div>
+              )}
+            </div>
+            {/* BOX */}
+
+            <div
+              className={cn(
+                'group flex h-10 w-10 shrink-0 cursor-pointer items-center justify-center overflow-hidden rounded-lg transition-all duration-200',
+                isSelected && 'bg-brand-500',
+                !isSelected && 'group-hover:bg-brand-500 bg-gray-700',
+              )}
+            >
+              {serverAvatarSrc ? (
+                <img src={serverAvatarSrc} alt="server avatar" className="h-full w-full object-cover object-center" />
+              ) : avatarFallback ? (
+                <p>{avatarFallback}</p>
+              ) : icon ? (
+                icon
+              ) : (
+                <img src="/src/assets/discord-mark-white.svg" alt="discord logo" className="w-[22.8px]" />
+              )}
+            </div>
           </button>
         </TooltipTrigger>
         {tooltip && (
-          <TooltipContent side="right">
+          <TooltipContent side="right" className="-translate-x-4">
             <p>{tooltip}</p>
           </TooltipContent>
         )}
       </Tooltip>
-    </div>
+    </>
   )
 }
--- apps/web/src/components/layouts/app-layout/app-sidenav/app-sidenav-servers-list.tsx
+++ apps/web/src/components/layouts/app-layout/app-sidenav/app-sidenav-servers-list.tsx
@@ -1,4 +1,5 @@
 import { AppSidenavServersListItem } from '@/components/layouts/app-layout/app-sidenav/app-sidenav-servers-list-item'
+import { PlusIcon } from 'lucide-react'
 
 export const AppSidenavServersList = () => {
   return (
@@ -6,6 +7,18 @@ export const AppSidenavServersList = () => {
       <AppSidenavServersListItem tooltip="Messages privés" />
       <div className="bg-gray-660 h-px w-6"></div>
       <AppSidenavServersListItem tooltip="Serveur d'Element" avatarFallback="SdE" />
+      {/* TODO: Add a modal to add a server */}
+      <AppSidenavServersListItem
+        tooltip="Ajouter un serveur"
+        icon={
+          <div className="flex size-5 shrink-0 items-center justify-center rounded-full bg-gray-200">
+            <PlusIcon
+              className="text-gray-630 group-hover:text-brand-500 size-4 transition-colors duration-200"
+              strokeWidth={3}
+            />
+          </div>
+        }
+      />
     </div>
   )
 }
