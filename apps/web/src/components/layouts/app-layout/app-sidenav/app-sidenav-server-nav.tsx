bd8ecd594c5172c9e4825ef97c48ca779fe8af55
Paul Bouloc <37270137+paul-bouloc@users.noreply.github.com>
2025-10-24 09:24:44 +0200

    add sidenav condition to display proper nav

---
+++ apps/web/src/components/layouts/app-layout/app-sidenav/app-sidenav-server-nav.tsx
@@ -0,0 +1,3 @@
+export const AppSidenavServerNav = () => {
+  return <div>AppSidenavServerNav</div>
+}
--- apps/web/src/components/layouts/app-layout/app-sidenav/app-sidenax.tsx
+++ apps/web/src/components/layouts/app-layout/app-sidenav/app-sidenax.tsx
@@ -1,11 +1,20 @@
 import { AppSidenavPrivateNav } from '@/components/layouts/app-layout/app-sidenav/app-sidenav-private-nav'
+import { AppSidenavServerNav } from '@/components/layouts/app-layout/app-sidenav/app-sidenav-server-nav'
 import { AppSidenavServersList } from '@/components/layouts/app-layout/app-sidenav/app-sidenav-servers-list'
+import { paths } from '@/config/paths'
+import { useLocation } from 'react-router'
 
 export const AppSidenav = () => {
+  const location = useLocation()
+
+  const isPrivateSpace = location.pathname.includes(paths.app.personal.path)
+  const isServerSpace = !isPrivateSpace
+
   return (
     <div className="flex max-w-[374px] flex-1 shrink-0 bg-gray-800">
       <AppSidenavServersList />
-      <AppSidenavPrivateNav />
+      {isPrivateSpace && <AppSidenavPrivateNav />}
+      {isServerSpace && <AppSidenavServerNav />}
     </div>
   )
 }
