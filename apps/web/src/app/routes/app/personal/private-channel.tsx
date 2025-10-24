902d6b7942e5f2edc7d96749501657efd5646569
Paul Bouloc <37270137+paul-bouloc@users.noreply.github.com>
2025-10-24 11:47:03 +0200

    fix breadcrumb for rivate channel page

--- apps/web/src/app/routes/app/personal/private-channel.tsx
+++ apps/web/src/app/routes/app/personal/private-channel.tsx
@@ -1,4 +1,15 @@
+import { DiscordIcon } from '@/components/icons/discord-icon'
+import { useBreadcrumb } from '@/contexts/breadcrumb'
+import { useEffect } from 'react'
+
 export default function PrivateChannelPage() {
+  const { setBreadcrumb } = useBreadcrumb()
+
+  useEffect(() => {
+    setBreadcrumb(DiscordIcon, 'Messages privés')
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div>
       <h1>PrivateChannelPage</h1>
